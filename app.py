8c6d5a75e319a49ba29bd981de73a78d26b1ea87
Saptarishi_Sarkar <77563125+Saptarishi-S@users.noreply.github.com>
2024-03-24 05:47:47 +0000

    updated the website a bit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from os import name
+from flask import Flask, render_template, request
+import time
 
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return "Hello World!"
-print(__name__)
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
 
+@app.route('/')
+def hello_World():
+  return render_template('index.html',)
+if __name__=='__main__':
+  app.run(host='0.0.0.0', debug=True)
+  
