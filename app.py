99b73cb1c3beb4b88ad232652b2f9dc56d4c8c93
Saptarishi_Sarkar <77563125+Saptarishi-S@users.noreply.github.com>
2024-03-25 04:43:15 +0000

    added some additional features and completed the task and task history thing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import time
 app = Flask(__name__)
 
 @app.route('/')
-def hello_World():
+def ht_ml_code():
   return render_template('index.html',)
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
